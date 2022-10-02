1f5dda61caccf4b1ca2cd68a2d38034de899da75
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-02 13:15:07 +0530

    Validated using regex

--- Simple Calculator/calc.js
+++ Simple Calculator/calc.js
@@ -1,3 +1,42 @@
 function Click(num) {
-  document.getElementById("text-box").value = num;
+  document.getElementById("text-box").value += num;
 }
+
+function equals() {
+  let expr = document.getElementById("text-box").value;
+  let flag = validate(expr);
+  if (flag == 1) {
+    calc(expr);
+  } else {
+    return;
+  }
+}
+
+function calc(expr) {
+  let operands = expr.split(/[^0-9\.]+/);
+  console.log(operands);
+  let operators = expr.split(/[0-9\.]+/).filter((x) => x);
+  console.log(operators);
+  let value;
+}
+
+function validate(value) {
+  const regEx = /^[\d\.]+([+*/-][\d\.]+)*$/;
+  if (!value.match(regEx)) {
+    alert("Invalid Input");
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+// $(document).ready(function () {
+//   regEx = /^\d+$/;
+//   $("#text-box").on("change", function () {
+//     if (regEx.test($(this).val())) {
+//       // Contain numbers only
+//     } else {
+//       alert("Invlaid Input");
+//     }
+//   });
+// });
