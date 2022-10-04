e3a2e25fa738af26c5bd54eb8c8cd32230140c24
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-04 18:14:18 +0530

    Episode 3

--- Angular6Project/src/app/app-routing.module.ts
+++ Angular6Project/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeeComponent } from './employee/list-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListEmployeeComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
