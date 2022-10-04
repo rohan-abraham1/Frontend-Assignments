058c1e21c647d4334842cc892a08dd8d290a2337
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-04 16:30:03 +0530

    Episode 3 - Created employee

--- Angular6Project/src/app/app.module.ts
+++ Angular6Project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
+import { ListEmployeeComponent } from './employee/list-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeeComponent
   ],
   imports: [
     BrowserModule,
