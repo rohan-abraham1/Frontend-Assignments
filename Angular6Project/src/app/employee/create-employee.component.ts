43e037855931d63721136515f0c3f3e3323010de
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-06 10:05:17 +0530

    Episode 5

--- Angular6Project/src/app/employee/create-employee.component.ts
+++ Angular6Project/src/app/employee/create-employee.component.ts
@@ -19,6 +19,10 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
   onSubmit(): void {
+    console.log(this.employeeForm.touched);
     console.log(this.employeeForm.value);
+
+    console.log(this.employeeForm.controls['fullName'].value);
+    console.log(this.employeeForm.get('fullName')?.value);
   }
 }
