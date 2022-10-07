7477f7274a11b97ff0314e3f3850ab4554276c8f
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-07 14:29:15 +0530

    Episode 12- Moved validation messages to component class

--- Angular6Project/src/app/employee/create-employee.component.ts
+++ Angular6Project/src/app/employee/create-employee.component.ts
@@ -12,8 +12,38 @@ export class CreateEmployeeComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   onLoadDataClick(): void {
-    this.logKeyValuePairs(this.employeeForm);
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
+    
   }
+
+  validationMessages= {
+    fullName : {
+      required: 'Full Name is required',
+      minlength: 'Full Name must be greater than 2 characters',
+      maxlength: 'Full Name must be less than 10 characters',
+    },
+    email: {
+      required: 'Email is required',
+    },
+    skillName: {
+      required: 'Skill Name is required',
+    },
+    experienceInYears: {
+      required: 'Experience is required',
+    },
+    proficiency: {
+      required: 'Proficiency is required',
+    },
+  };
+
+  formErrors = {
+    fullName: '',
+    email: '',
+    skillName: '',
+    experienceInYears: '',
+    proficiency: '',
+  };
 
   ngOnInit(): void {
     this.employeeForm = this.fb.group({
@@ -25,23 +55,31 @@ export class CreateEmployeeComponent implements OnInit {
           Validators.maxLength(20),
         ],
       ],
-      email: [''],
+      email: ['', Validators.required],
       skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['beginner'],
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required],
       }),
     });
   }
 
-  logKeyValuePairs(group: FormGroup): void {
+  logValidationErrors(group: FormGroup): void {
     console.log(
       Object.keys(group.controls).forEach((key: string) => {
         const abstractControl = group.get(key);
         if (abstractControl instanceof FormGroup) {
-          this.logKeyValuePairs(abstractControl);
+          this.logValidationErrors(abstractControl);
         } else {
-          abstractControl?.disable();
+          (this.formErrors as any)[key] = '';
+          if (abstractControl && !abstractControl.valid) {
+            const messages = (this.validationMessages as any)[key];
+            for (const errorKey in abstractControl.errors){
+              if(errorKey){
+                (this.formErrors as any)[key] += messages[errorKey] + ' ';
+              }
+            }
+          }
         }
       })
     );
@@ -55,3 +93,18 @@ export class CreateEmployeeComponent implements OnInit {
     console.log(this.employeeForm.get('fullName')?.value);
   }
 }
+
+
+// type validationMessagesType = {
+//   [key in modelTypes] : validationType
+// }
+
+// type validationType = {
+//   required?: string;
+//   minlength?: string;
+//   maxlength?: string;
+// }
+
+// enum modelTypes {
+//   fullname = 'fullname'
+// }
