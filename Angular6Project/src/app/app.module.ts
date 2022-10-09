23f8694aa13dcec55ec4c806da1ce0ec973adfe1
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-09 16:42:20 +0530

    Episode 26 - Started with RxJS operators

--- Angular6Project/src/app/app.module.ts
+++ Angular6Project/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './employee/create-employee.component';
 import { ListEmployeeComponent } from './employee/list-employee.component';
+import { EmployeeService } from './employee/employee.service';
 
 @NgModule({
   declarations: [AppComponent, CreateEmployeeComponent, ListEmployeeComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [EmployeeService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Angular6Project/src/app/employee/list-employee.component.ts
+++ Angular6Project/src/app/employee/list-employee.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { IEmployee } from './IEmployee';
 
 @Component({
   selector: 'app-list-employee',
   templateUrl: './list-employee.component.html',
-  styleUrls: ['./list-employee.component.css']
+  styleUrls: ['./list-employee.component.css'],
 })
 export class ListEmployeeComponent implements OnInit {
+  employees!: IEmployee[];
 
-  constructor() { }
+  constructor(private _employeeService: EmployeeService) {}
 
   ngOnInit(): void {
+    this._employeeService.getEmployees().subscribe(
+      (listEmployees) => (this.employees = listEmployees),
+      (err) => console.log(err)
+    );
   }
-
 }
