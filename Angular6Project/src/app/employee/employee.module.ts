5f1676eefad905f9f097cb42f6223325ce48937c
Rohan Abraham <68459206+rohan-abraham1@users.noreply.github.com>
2022-10-12 09:45:27 +0530

    Episode 34 - Creating shared modules

--- Angular6Project/src/app/employee/employee.module.ts
+++ Angular6Project/src/app/employee/employee.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
 
 import { CreateEmployeeComponent } from './create-employee.component';
 import { ListEmployeeComponent } from './list-employee.component';
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [CreateEmployeeComponent, ListEmployeeComponent],
-  imports: [CommonModule, ReactiveFormsModule, EmployeeRoutingModule],
+  imports: [EmployeeRoutingModule, SharedModule],
 })
 export class EmployeeModule {}
